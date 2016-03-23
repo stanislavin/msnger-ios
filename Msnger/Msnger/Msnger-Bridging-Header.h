611fe57fc1a8cf199fd7af6063c16164d57a35a3
Stanislav Slavin <stanislav.slavin@motorolasolutions.com>
2016-03-23 21:05:13 +0300

    initial commit

---
+++ Msnger/Msnger/Msnger-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Msnger-Bridging-Header.h
+//  Msnger
+//
+//  Created by Stanislav Slavin on 23/03/16.
+//  Copyright © 2016 Stanislav Slavin. All rights reserved.
+//
+
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+
