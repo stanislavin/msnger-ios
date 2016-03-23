14bdd3ab08b8eccebde8eec6f3fa23b9fa53cef7
Stanislav Slavin <stanislav.slavin@motorolasolutions.com>
2016-03-23 23:43:35 +0300

    included core sources into project, curl TBD

--- Msnger/Msnger/Msnger-Bridging-Header.h
+++ Msnger/Msnger/Msnger-Bridging-Header.h
@@ -10,4 +10,4 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-
+#import "../../../msnger-core/wrapper/Msnger.h"
--- Msnger/Msnger/ViewController.swift
+++ Msnger/Msnger/ViewController.swift
@@ -13,6 +13,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let msnger:Msnger = Msnger()
+        let number = "79213276120".dataUsingEncoding(NSUTF8StringEncoding)!
+        let message = "Hello from swift!".dataUsingEncoding(NSUTF8StringEncoding)!
+        let lat = 0.0
+        let lon = 0.0
+        sendMessage(msnger, UnsafeMutablePointer(number.bytes), UnsafeMutablePointer(message.bytes), lat, lon,
+            {
+                code in
+                let _ = code
+            })
     }
 
     override func didReceiveMemoryWarning() {
